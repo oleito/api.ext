2fc361a5477ce11ef1ecb5b9a6a06609d5c480a7
Leandro <oleito@gmail.com>
2019-12-04 22:49:26 -0300

    gitflow-feature-stash: token&basic

---
+++ src/classes/peticion.php
@@ -0,0 +1,141 @@
+<?php
+class peticion
+{
+    private $request = null;
+    private $response = null;
+
+    public function __construct($request, $response)
+    {
+        $this->request = $request;
+        $this->response = $response;
+    }
+
+    public function conToken($funcionAnonina)
+    {
+        $token = new token;
+        if ($this->request->hasHeader('HTTP_TOKEN') && $token->checkToken($this->request->getHeader("HTTP_TOKEN")[0])) {
+            $body["HTTP_TOKEN"] = $this->request->getHeader("HTTP_TOKEN")[0];
+
+            $body = array(
+                'token' => $token->setToken("datosUsuario"),
+                'body' => $funcionAnonina
+            );
+
+            return $this->response
+                ->withHeader('Content-type', 'application/json')
+                ->withStatus(200)
+                ->withJson($body);
+        } else {
+            return $this->response
+                ->withHeader('Content-type', 'application/json')
+                ->withStatus(401);
+        }
+    }
+    public function sinToken($funcionAnonina)
+    {
+
+    }
+
+    public function getRequest($anonima)
+    {
+        //declaracion de variables.
+        $tokenIn = null;
+        $tokenOut = null;
+        $mensajesPendientes = null;
+        $body = null;
+        $tokenIn = $this->request->getHeaderLine('token');
+        if ($tokenIn != "" && $tokenIn) {
+            $token = new token();
+            if ($token->checkToken($tokenIn)) {
+                //una vez aprobado el token, podemos seguir operando.
+                //trata de hacer la consulta correspondiente
+                //en caso de error, devuelve el informe correspo
+                try {
+                    /** Asigna al $body el contenido de la funcion anonima**/
+                    $body = $anonima;
+                    /* Busca nuevos mensajes pendientes. */
+                    //$mensajesPendientes = new mensajesPendientes();
+                    //$mensajesPendientes = $mensajesPendientes->getMensajesPendientes();
+                    $msgs = array();
+                    array_push($msgs, ['from' => 'lean', 'msg' => "chau"]);
+                    array_push($msgs, ['from' => 'x', 'msg' => "d"]);
+                    $tokenOut = $token->updateToken($tokenIn);
+                    $resp = array(
+                        'token' => $tokenOut,
+                        #'msjs' => $msgs,
+                        #'msjQ' => count($msgs),
+                        'body' => $body,
+                    );
+                    //$resp = json_encode($resp, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+                    $resp = json_encode($resp);
+                    return $this->response
+                        ->withStatus(200)
+                        ->write($resp);
+                    //->write($resp);
+                } catch (PDOException $e) {
+                    $resp = array(
+                        'token' => null,
+                        'body' => print_r($e->getMessage()),
+                    );
+                    $resp = json_encode($resp, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+                    return $this->response
+                        ->withStatus(500)
+                        ->write($resp);
+                }
+            } else {
+                $resp = array(
+                    'token' => null,
+                    'body' => null,
+                );
+                $resp = json_encode($resp, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+                return $this->response
+                    ->withStatus(401)
+                    ->write(null);
+            }
+        } else {
+            $resp = array(
+                'token' => null,
+                'body' => null,
+            );
+            $resp = json_encode($resp, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+            return $this->response
+                ->withStatus(402)
+                ->write(null);
+        }
+    }
+    public function getRequestSinToken($anonima)
+    {
+        //declaracion de variables.
+        try {
+            /** Asigna al $body el contenido de la funcion anonima**/
+            $body = $anonima;
+            /* Busca nuevos mensajes pendientes. */
+            //$mensajesPendientes = new mensajesPendientes();
+            //$mensajesPendientes = $mensajesPendientes->getMensajesPendientes();
+            $msgs = array();
+            array_push($msgs, ['from' => 'lean', 'msg' => "chau"]);
+            array_push($msgs, ['from' => 'x', 'msg' => "d"]);
+            $resp = array(
+                'token' => 'SinToken',
+                #'msjs' => $msgs,
+                #'msjQ' => count($msgs),
+                'body' => $body,
+            );
+            //$resp = json_encode($resp, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+            $resp = json_encode($resp);
+            return $this->response
+                ->withStatus(200)
+                ->write($resp);
+            //->write($resp);
+        } catch (PDOException $e) {
+            $resp = array(
+                'token' => null,
+                'body' => print_r($e->getMessage()),
+            );
+            $resp = json_encode($resp, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+            return $this->response
+                ->withStatus(500)
+                ->write($resp);
+        }
+    }
+}
---
+++ src/classes/token.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ *
+ */
+date_default_timezone_set('America/Argentina/San_Luis');
+
+class token
+{
+    //Declaracion de variables
+    private $secret = 'postventa2019';
+    private $timeOut = 10;
+    private $expire = null;
+    /*  */
+    public function __construct()
+    {
+        $now = strtotime('now');
+        $this->expire = $now + ($this->timeOut * 60);
+    }
+    private function base64url_encode($data)
+    {
+        return rtrim(strtr(base64_encode($data), "+/", "-_"), "=");
+    }
+    /**
+     * Recibe un arreglo y devuelve un token que contiene los datos del usuario
+     * id_usuario
+     * usuario_nombre
+     * usuario_correo
+     * access_level
+     */
+    public function setToken($usuario)
+    {        
+        return "Token de prueba";
+        try {
+            //Define los Headers del token
+            $header = self::base64url_encode(json_encode([
+                "alg" => "HS256",
+                "typ" => "JWT",
+            ]));
+            //Define el payload y carga los datos del usuario
+            $payload = self::base64url_encode(json_encode([
+                "expire" => $this->expire, //esta linea dejarla asi, xq sino rompe
+                "id" => $usuario['idusuario'],
+                "nombre" => $usuario['persona']['persona_nombres'],
+                "email" => $usuario['persona']['persona_email'],
+                "grupos" => $usuario['grupos'],
+            ]));
+            $signature = self::base64url_encode(hash_hmac("sha256", $header . "." . $payload, $this->secret, true));
+            //Construye el token = header+paylad+signature
+            $token = $header . "." . $payload . "." . $signature;
+
+            return $token;
+        } catch (\Throwable $th) {
+            return null;
+        }
+    }
+    /**
+     * Verifica si el token es Valito
+     * @param  [] $token [token que envia el frontend]
+     * @return [bool]        [Verdadero si es valido]
+     */
+    public function checkToken($token)
+    {
+
+       return true;################ dev prop
+
+
+
+        try {
+            //Divide el Token en 3 Partes
+            $t = explode(".", $token);
+            //asigna los valores a referencias
+            $refHeader = $t[0];
+            $refPayload = $t[1];
+            $refSignature = $t[2];
+            //Crea un nuevo signature con los datos del token
+            $newSignature = self::base64url_encode(hash_hmac("sha256", $refHeader . "." . $refPayload, $this->secret, true));
+            //decodifica el payload
+            $refPayload = json_decode(base64_decode($refPayload), true);
+            //obtiene el momento en que expira
+            $expire = $refPayload['expire'];
+            //Verifica si el token ha expirado
+            if ($expire >= strtotime('now') && $refSignature === $newSignature) {
+                // si aun es valido
+                return true;
+            } else {
+                // Si ya expiro
+                return false;
+            }
+        } catch (\Throwable $th) {
+            // Si ya expiro
+            return false;
+        }
+    }
+    /**
+     * Renueva un token existente
+     * @param  [] $token [token que envia el frontend]
+     * @return [bool]        [Verdadero si es valido]
+     */
+    public function updateToken($token)
+    {
+        try {
+            //Divide el Token en 3 Partes
+            $t = explode(".", $token);
+            //asigna los valores a referencias
+            $refHeader = $t[0];
+            $refPayload = $t[1];
+            //decodifica el payload
+            $refPayload = json_decode(base64_decode($refPayload), true);
+            //actualiza EXPIRE
+            $refPayload['expire'] = $this->expire;
+            //codificamos el nuevo Payload con
+            $newPayload = self::base64url_encode(json_encode($refPayload));
+            //Crea un nuevo signature con los datos del token
+            $signature = self::base64url_encode(hash_hmac("sha256", $refHeader . "." . $newPayload, $this->secret, true));
+            //Construye el token = header+paylad+signature
+            $newToken = $refHeader . "." . $newPayload . "." . $signature;
+            return $newToken;
+        } catch (\Throwable $th) {
+            return false;
+            
+        }
+    }
+}
